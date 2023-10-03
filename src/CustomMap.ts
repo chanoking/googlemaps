9f0e858b1d2bea08ed75ab6cddceb512a41582d9
진찬호 <chanhojin94@kakao.com>
2023-10-03 14:09:29 +0900

    google maps

---
+++ src/CustomMap.ts
@@ -0,0 +1,50 @@
+/// <reference types="@types/google.maps" />
+import { Company } from "./Company";
+import { User } from "./User";
+
+// Instructions toevery other class
+// on how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener("click", () => {
+      const inforWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      inforWindow.open(this.googleMap, marker);
+    });
+  }
+}
